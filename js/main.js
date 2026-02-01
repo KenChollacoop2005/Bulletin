02d0ed6b1296833c13e702416c42833afb8aef9f
Ken <kenchollacoop@gmail.com>
2026-02-01 10:45:01 -0500

    Fixed 404 issue
    also fixed the spelling of accessibility

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const posterFiles = [
   "Posters/Pamphlet-SawasdeeDC.html",
   "Posters/Poster2.html",
   "Posters/Poster3.html",
-  "posters/AcessibilityPaper.html",
+  "Posters/AccessibilityPaper.html",
   "Posters/NameCard.html",
   "Posters/CD-Saturn.html",
 ];
